fbb6a3044d3fc16fcfd3c2284841690aa7c8c061
Raju Gautam <raju.rachana@gmail.com>
2017-04-24 16:20:06 +0545

    Added add/edit/delete in html form.

--- script/js/scripts.js
+++ script/js/scripts.js
@@ -1,77 +1,106 @@
-function init() {
-    
+let table           = document.querySelector("#mytable");
+var btnAdd          = document.querySelector("#btnAdd");
+let btnSave         = document.querySelector("#btnSave");
+let removeAllButton = document.querySelector("#btnRemoveAll");
+
+let form            = document.myForm;
+
+var serialNumber    = 1;
+
+// hide save button
+btnSave.style.display = 'none';
+
+function Employee(){
+    this.name = null;
+    this.email = null;
+    this.address = null;
+
+    this.addNew = function(){
+        let tr = document.createElement("TR");
+        let newRow = '<th>' + serialNumber + '</th>'+
+                        '<td id="name' + serialNumber + '">' + this.name + '</td>'+
+                        '<td id="email' + serialNumber + '">' + this.email + '</td>'+
+                        '<td id="address' + serialNumber + '">' + this.address + '</td>'+
+                        '<td><a href="javascript:;" class="edit" id="edit-' + serialNumber + '">Edit</a> | <a href="javascript:;" class="delete" id="delete-' + serialNumber + '">Delete</a></td>';
+        tr.innerHTML = newRow;
+
+        table.appendChild(tr);
+        serialNumber++;
+        this.clearForm();
+    }
+
+    this.clearForm = function(){
+        form.reset();
+    }
+
+    this.deleteRow = function(element){
+        if(confirm("Are you sure to delete ?\nPress OK to confirm, or Cancel.")){
+            var tr = element.parentElement.parentElement;
+            tr.remove();
+
+            // tr.parentElement.removeChild(tr);
+            
+            // tr.outerHTML = "";
+            // delete tr;
+        }
+
+        return false;
+    }
+
+    this.editRow = function(element){
+        var id = element.id.substring(5);
+
+        var name = document.querySelector("#name" + id).textContent;
+        var email = document.querySelector("#email" + id).textContent;
+        var address = document.querySelector("#address" + id).textContent;
+
+        document.myForm.name.value = name;
+        document.myForm.email.value = email;
+        document.myForm.address.value = address;
+
+        btnSave.style.display = '';
+        btnAdd.style.display = 'none';
+    }
+
+    this.editSave = function(){
+        btnSave.style.display = 'none';
+        btnAdd.style.display = '';
+
+        this.clearForm();
+    }
+
+    this.removeAll = function(){
+        table.innerHTML = "";
+    }
 }
 
-// function doit() {
-//     alert(this.parentNode.className);
-// }
+var employee = new Employee();
 
-window.onload = init;
+// add event listener for edit and delete
+document.addEventListener('click', function(event){
+    if(!event.target) return;
 
-var table = document.querySelector("#mytable");
+    var element = event.target;
 
+    if(element.className == 'edit'){
+        employee.editRow(element);
+    }
 
-
-// function enableEditClick(){
-//     var edit = document.querySelector(".edit");
-    
-// }
-
-var edit = document.querySelector(".edit");
-if(edit && edit.length){
-    edit.onclick = function(){
-        alert("Edit clicked");
-    }    
-}
-
+    if(element.className == "delete"){
+        employee.deleteRow(element);
+    }
+});
 
 // Remove All
-var removeAllButton = document.querySelector("#btnRemoveAll");
 removeAllButton.onclick = function(){
-    table.innerHTML = "";
+    employee.removeAll();
 }
 
-// var table = document.getElementById("mytable");
-// var divs = document.getElementsByTagName("div");
-// var menu = document.getElementsByClassName("navigation");
+// Add new employee
+btnAdd.onclick = function(){ // anonymous function
+    employee.name = form.name.value;
+    employee.email = form.email.value;
+    employee.address = form.address.value;
 
-var serialNumber = 10;
-
-var data = {};
-
-function setValues(){
-    data.name = document.myForm.name.value;
-    data.email = document.myForm.email.value;
-    data.address = document.myForm.address.value;
-}
-
-function clearForm(){
-    document.myForm.name.value = "";
-    document.myForm.email.value = "";
-    document.myForm.address.value = "";
-}
-
-var submitButton = document.querySelector("#btnSubmit");
-
-submitButton.onclick = myfunc;
-
-var myfunc = function(){ // anonymous function
-    setValues();
-
-    this.value = "Saved";
-    this.style.display = "none";
-
-    let existingRows = table.innerHTML;
-
-    let newRow = '<tr>'+
-                    '<th>' + serialNumber + '</th>'+
-                    '<th id="name' + serialNumber + '">' + data.name + '</th>'+
-                    '<th id="email' + serialNumber + '">' + data.email + '</th>'+
-                    '<th id="address' + serialNumber + '">' + data.address + '</th>'+
-                    '<th><a href="#" class="edit" id="edit-' + serialNumber + '">Edit</a> | <a href="#" id="delete-' + serialNumber + '">Delete</a></th>'+
-                '</tr>';
-    table.innerHTML = existingRows + newRow;
-    serialNumber++;
-    // enableEditClick();
-    clearForm();
+    employee.addNew();
 }
